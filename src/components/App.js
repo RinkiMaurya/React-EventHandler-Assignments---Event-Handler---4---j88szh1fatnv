213154d597784c7e4e2b5c11f738e014d55b562c
RinkiMaurya <96375337+RinkiMaurya@users.noreply.github.com>
2022-04-08 11:44:37 +0530

    Str

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,16 @@ import React from "react";
 import "../styles/App.css";
 const App = () => {
   const handleDoubleClick = (event) => {
-    event.preventDefault();
+    // event.preventDefault();
     if (event.detail == 2) {
       console.log("I was double clicked ");
     } else {
       console.log("I was not double clicked");
     }
   };
-  // const handleSingleClick = (event) => {
-  //   event.preventDefault();
-  //   console.log("I was not double clicked ");
-  // };
-
   return (
     <div id="main">
-      <button
-        id="dblclick-btn"
-        onClick={handleDoubleClick}
-        // onClick={handleSingleClick}
-      >
+      <button id="dblclick-btn" onClick={handleDoubleClick}>
         Double click me
       </button>
     </div>
